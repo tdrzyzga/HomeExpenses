0a926166487f55b4cf9bc57900f27ea7d302704e
Tomasz Drzyzga <tomasz.drzyzga@iteo.co>
2017-12-13 13:40:24 +0100

    EDIT: Modified startup configuration

--- HomeExpenses.Domain/Startup.cs
+++ HomeExpenses.Domain/Startup.cs
@@ -2,12 +2,26 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 
 namespace HomeExpenses.Domain
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
+
+        protected Startup()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true);
+
+            Configuration = configurationBuilder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<HomeExpensesDbContext>(options =>
