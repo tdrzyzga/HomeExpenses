601fc1d0f5eca3b13bebaa54f0ecbfd00cdbabbf
Tomasz Drzyzga <tomasz.drzyzga@iteo.co>
2017-12-15 13:25:28 +0100

    ADD: ICommand and BaseCommand

---
+++ Core.Message/Command/BaseCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Message.Command
+{
+    public abstract class BaseCommand : ICommand
+    {
+        public Metadata Metadata { get; set; }
+
+        public virtual void SetMetadata(Metadata metadata)
+        {
+            Metadata = metadata;
+        }
+    }
+}
---
+++ Core.Message/Command/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Message.Command
+{
+    public interface ICommand
+    {
+        Metadata Metadata { get; }
+
+        void SetMetadata(Metadata metadata);
+    }
+}
