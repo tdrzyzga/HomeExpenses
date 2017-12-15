c7602cbfc8e03ead4af62417899ba90c30c5e2eb
Tomasz Drzyzga <tomasz.drzyzga@iteo.co>
2017-12-15 20:15:52 +0100

    ADD: BillCommanActor

---
+++ HomeExpenses.Application/Bill/BillCommandActor.cs
@@ -0,0 +1,14 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeExpenses.Application.Bill
+{
+    public class BillCommandActor : ReceiveActor
+    {
+        protected BillCommandActor()
+        {
+        }
+    }
+}
