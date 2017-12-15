d2d77a52ea8c2c15a0867b693183635daefed311
Tomasz Drzyzga <tomasz.drzyzga@iteo.co>
2017-12-15 13:07:29 +0100

    EDIT: Startup in webapi

---
+++ HomeExpenses.WebApi/HomeExpensesWebApiModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Core.Akka;
+using Core.Akka.ActorSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeExpenses.WebApi
+{
+    public class HomeExpensesWebApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule<CoreAkkaModule>();
+            builder.Register(ctx => new LocalActorSystemManager("MainActorSystem")).AsImplementedInterfaces().SingleInstance();
+        }
+    }
+}
--- HomeExpenses.WebApi/Startup.cs
+++ HomeExpenses.WebApi/Startup.cs
@@ -2,28 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Akka.DI.AutoFac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Autofac.Extensions.DependencyInjection;
+using Core.Akka.ActorSystem;
 
 namespace HomeExpenses.WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IContainer DiContainer { get; private set; }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConfiguration>(Configuration);
+
             services.AddMvc();
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<HomeExpensesWebApiModule>();
+            builder.Populate(services);
+            DiContainer = builder.Build();
+
+            new AutoFacDependencyResolver(DiContainer, DiContainer.Resolve<ILocalActorSystemManager>().ActorSystem);
+
+            return new AutofacServiceProvider(DiContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
