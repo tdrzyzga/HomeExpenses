0e276727d95fbf0675768cf3b5595e947e4dd0f1
Tomasz Drzyzga <tomasz.drzyzga@iteo.co>
2017-12-15 17:16:47 +0100

    ADD: Remote provider

--- HomeExpenses.WebApi/Infrastructure/Controller/BaseController.cs
+++ HomeExpenses.WebApi/Infrastructure/Controller/BaseController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Localization;
+﻿using Core.Akka.ActorSystem;
+using Core.Message;
+using Core.Message.Command;
+using HomeExpenses.WebApi.Infrastructure.Seed;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +14,23 @@ namespace HomeExpenses.WebApi.Infrastructure.Controller
     public class BaseController : Microsoft.AspNetCore.Mvc.Controller
     {
         private readonly IServiceProvider _serviceProvider;
+        private readonly ILocalActorSystemManager _localActorSystemManager;
 
-        public BaseController(BaseControllerPayload payload)
+        public BaseController(IServiceProvider serviceProvider, ILocalActorSystemManager localActorSystemManager)
         {
-            _serviceProvider = payload.ServiceProvider;
+            _serviceProvider = serviceProvider;
+            _localActorSystemManager = localActorSystemManager;
+        }
+
+
+        protected async Task<IActionResult> SendCommand<TCommand>(string dispatcherActorName, TCommand command) where TCommand : ICommand
+        {
+            var culture = GetCulture();
+            command.SetMetadata(new Metadata(culture, FakeSeedData.UserId));
+
+            var actor = _localActorSystemManager.ActorSystem.
+
+            return BadRequest();
         }
 
 
