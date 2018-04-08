c0296f5cf908c0b6fc212636b620c7d3c003ba20
tdrzyzga <tdrzyzga@gmail.com>
2018-04-08 20:42:00 +0200

    ADD: Axious for user interface

--- HomeExpenses.UI/home-expenses-ui/src/components/About.vue
+++ HomeExpenses.UI/home-expenses-ui/src/components/About.vue
@@ -1,12 +1,47 @@
 <template>
-  <div>
-About view
-  </div>
+  <v-form v-model="valid" ref="form" lazy-validation>
+    <v-text-field
+      label="Name"
+      v-model="name"
+      :rules="nameRules"
+      :counter="10"
+      required
+    ></v-text-field>
+    <v-btn
+      @click="submit"
+      :disabled="!valid"
+    >
+      submit
+    </v-btn>
+    <v-btn @click="clear">clear</v-btn>
+  </v-form>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'About'
+  name: 'About',
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+    ]
+  }),
+  methods: {
+    submit () {
+      if (this.$refs.form.validate()) {
+        // Native form submission is not yet supported
+        axios.post('http://localhost:60440/api/values', {
+          Name: this.name
+        })
+      }
+    },
+    clear () {
+      this.$refs.form.reset()
+    }
+  }
 }
 </script>
 <!-- styling for the component -->
