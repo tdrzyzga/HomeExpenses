ec9172b7474fe03fd800ded2c51dda90300e7727
tdrzyzga <tdrzyzga@gmail.com>
2018-07-04 21:37:25 +0200

    ADD: Added creating recipient

---
+++ HomeExpenses.Application/Recipients/CreateRecipientCommandActor.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Core.Akka.ActorAutostart;
+using Core.Application.Actor;
+using Core.Domain.Repository;
+using Core.Domain.ValueObjects;
+using HomeExpenses.Domain.Recipients.Factory;
+using HomeExpenses.Domain.Recipients.Model;
+using HomeExpenses.Message.Recipients.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace HomeExpenses.Application.Recipients
+{
+    [AutostartActor("CreateRecipientCommandActor")]
+    public class CreateRecipientCommandActor : BaseActor
+    {
+        private readonly IRecipientFactory _recipientFactory;
+        private readonly IRepository<Recipient> _recipientRepository;
+
+        public CreateRecipientCommandActor(ILogger<CreateRecipientCommandActor> logger, IRecipientFactory recipientFactory, IRepository<Recipient> recipientRepository)
+            : base(logger)
+        {
+            _recipientFactory = recipientFactory;
+            _recipientRepository = recipientRepository;
+
+            ReceiveAsync<CreateRecipientCommand>(Handle);
+        }
+
+        private async Task Handle(CreateRecipientCommand command)
+        {
+            await HandleCommand(command, async x =>
+            {
+                var address = new AddressValueObject(command.City, command.Street, command.Number);
+
+                var recipient = await _recipientFactory.Create(command.Id, command.Metadata.TenantId, command.Name, address);
+
+                await _recipientRepository.Save(recipient);
+            });
+        }
+    }
+}
