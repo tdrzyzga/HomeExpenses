da99b27eaf576fd3b87be937492df541d9dc3c57
tdrzyzga <tdrzyzga@gmail.com>
2018-07-04 21:48:26 +0200

    ADD: Controllers for periodic expenses and recipients

---
+++ HomeExpenses.WebApi/Controllers/RecipientsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Akka.ActorSystem;
+using HomeExpenses.Message.Recipients.Commands;
+using HomeExpenses.WebApi.Infrastructure.Controller;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeExpenses.WebApi.Controllers
+{
+    [Route("api/Recipients")]
+    public class RecipientsController : BaseController
+    {
+        public RecipientsController(IServiceProvider serviceProvider, ILocalActorSystemManager localActorSystemManager, ActorSystemConfiguration actorSystemConfiguration)
+            : base(serviceProvider, localActorSystemManager, actorSystemConfiguration)
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRecipient([FromBody] CreateRecipientCommand command)
+        {
+            return await SendCommand(command);
+        }
+    }
+}
