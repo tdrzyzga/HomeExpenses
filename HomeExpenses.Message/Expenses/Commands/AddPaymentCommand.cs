43f68aa038e331e5f7ccfeea44f67e45fd19d57b
tdrzyzga <tdrzyzga@gmail.com>
2018-07-04 21:37:44 +0200

    ADD: Added creating payment

---
+++ HomeExpenses.Message/Expenses/Commands/AddPaymentCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Core.Message.Command;
+
+namespace HomeExpenses.Message.Expenses.Commands
+{
+    public class AddPaymentCommand : BaseCommand
+    {
+        public Guid Id { get; }
+        public Guid ExpenseId { get; }
+        public Guid? RecipientId { get; }
+        public decimal Amount { get; }
+
+        public AddPaymentCommand(Guid id, Guid expenseId, Guid? recipientId, decimal amount)
+        {
+            Id = id;
+            ExpenseId = expenseId;
+            RecipientId = recipientId;
+            Amount = amount;
+        }
+    }
+}
