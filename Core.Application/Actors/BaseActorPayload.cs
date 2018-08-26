9fff6fb1f2981b1c4b49b01231fdc98369547c13
tdrzyzga <tdrzyzga@gmail.com>
2018-08-26 15:11:39 +0200

    ADD: Interface for BaseActorPayload

--- Core.Application/Actors/BaseActorPayload.cs
+++ Core.Application/Actors/BaseActorPayload.cs
@@ -14,7 +14,7 @@ namespace Core.Application.Actors
         public ILogger Logger { get; }
         public IServiceProvider ServiceProvider { get; }
 
-        public BaseActorPayload(ILogger logger, IServiceProvider serviceProvider)
+        public BaseActorPayload(ILogger<BaseActor> logger, IServiceProvider serviceProvider)
         {
             Logger = logger;
             ServiceProvider = serviceProvider;
