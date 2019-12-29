e0039c6b94bd40ad9a365cfb97fbd05830d55ba1
tdrzyzga <tdrzyzga@gmail.com>
2019-12-29 11:55:37 +0100

    ADD: GetRecipientDetailsQueryHandler and GetRecipientListQueryHandler

---
+++ HomeExpenses.Presentation/Recipients/QueryHandlers/GetRecipientDetailsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Core.Presentation.Handlers;
+using Core.Presentation.Repositories;
+using HomeExpenses.Domain.Recipients.Model;
+using HomeExpenses.Message.Recipients.Queries;
+using System.Threading.Tasks;
+
+namespace HomeExpenses.Presentation.Recipients.QueryHandlers
+{
+    public class GetRecipientDetailsQueryHandler : IQueryHandler<GetRecipientDetailsQuery, GetRecipientDetailsResult>
+    {
+        private readonly IReadOnlyRepository<Recipient> _recipientRepository;
+
+        public GetRecipientDetailsQueryHandler(IReadOnlyRepository<Recipient> recipientRepository)
+        {
+            _recipientRepository = recipientRepository;
+        }
+
+        public async Task<GetRecipientDetailsResult> Handle(GetRecipientDetailsQuery query)
+        {
+            var recipient = await _recipientRepository.Get(query.Id, query.Metadata.TenantId);
+
+            if (recipient == null)
+            {
+                return null;
+            }
+
+            return new GetRecipientDetailsResult(recipient.Id, recipient.Name, recipient.Address.City, recipient.Address.Street, recipient.Address.Number);
+        }
+    }
+}
---
+++ HomeExpenses.Presentation/Recipients/QueryHandlers/GetRecipientListQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Core.Presentation.Handlers;
+using Core.Presentation.Pagination;
+using Core.Presentation.Repositories;
+using HomeExpenses.Domain.Recipients.Model;
+using HomeExpenses.Message.Recipients.Queries;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeExpenses.Presentation.Recipients.QueryHandlers
+{
+    public class GetRecipientListQueryHandler : IQueryHandler<GetRecipientListQuery, GetRecipientListResult>
+    {
+        private readonly IReadOnlyRepository<Recipient> _recipientRepository;
+
+        public GetRecipientListQueryHandler(IReadOnlyRepository<Recipient> recipientRepository)
+        {
+            _recipientRepository = recipientRepository;
+        }
+
+        public async Task<GetRecipientListResult> Handle(GetRecipientListQuery query)
+        {
+            var recipients = _recipientRepository.GetPagedData(r => true,
+                                                                          query.PageIndex,
+                                                                          query.ItemsPerPage,
+                                                                          query.SortBy,
+                                                                          query.SortDir.ToSortDirection(),
+                                                                          query.Metadata.TenantId);
+
+            var totalItems = _recipientRepository.GetTotalItemsCount(r => true, query.Metadata.TenantId);
+
+            await Task.WhenAll(recipients, totalItems);
+
+            var recipientDtos = recipients.Result.Select(r => new GetRecipientListResult.RecipientDto(r.Id, r.Name, r.Address.City, r.Address.Street, r.Address.Number));
+
+            return new GetRecipientListResult(recipientDtos.ToImmutableArray(), (int)totalItems.Result);
+        }
+    }
+}
